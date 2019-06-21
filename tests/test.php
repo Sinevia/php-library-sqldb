a925c0b24b8b45d2fab44a53ba8e339f3b429989
Sinevia <info@sinevia.com>
2019-06-21 22:01:01 +0100

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -3,62 +3,7 @@
 define("ENVIRONMENT", 'testing');
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-
-/**
- * Returns a database instance
- * @return \Sinevia\SqlDb
- */
-function dbSqlite()
-{
-    static $db = null;
-    if (is_null($db)) {
-        $db = new \Sinevia\SqlDb(array(
-            'database_type' => 'sqlite',
-            'database_host' => ":memory:",
-            'database_name' => ":memory:",
-            'database_user' => "root",
-            'database_pass' => "",
-        ));
-    }
-    return $db;
-}
-
-/**
- * Returns a database instance
- * @return \Sinevia\SqlDb
- */
-function dbMySql()
-{
-    static $db = null;
-    if (is_null($db)) {
-        $db = new \Sinevia\SqlDb(array(
-            'database_type' => 'mysql',
-            'database_host' => "localhost",
-            'database_name' => "test",
-            'database_user' => "root",
-            'database_pass' => "",
-        ));
-    }
-    return $db;
-}
-
-/**
- * Returns a table instance
- * @return \Sinevia\SqlDb
- */
-function tableSqlite()
-{
-    return dbSqlite()->table('tests');
-}
-
-/**
- * Returns a table instance
- * @return \Sinevia\SqlDb
- */
-function tableMySql()
-{
-    return dbMySql()->table('tests');
-}
+require __DIR__ . '/helper.php';
 
 $tf = new \Testify\Testify("SqlDb Test Suite");
 
