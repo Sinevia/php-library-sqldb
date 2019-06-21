d03c8b6c4d528e903c7a955d7d139d087463e162
Sinevia <info@sinevia.com>
2019-06-21 22:53:14 +0100

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -10,7 +10,7 @@ $tf = new \Testify\Testify("SqlDb Test Suite");
 $tf->beforeEach(function ($tf) {
     tableSqlite()->drop();
 
-    tableSqlite()->column('Id', 'INTEGER', 'PRIMARY AUTOINCREMENT')
+    tableSqlite()->column('Id', 'INTEGER', 'AUTOINCREMENT')
         ->column('FirstName', 'STRING')
         ->column('MiddleNames', 'STRING')
         ->column('LastName', 'STRING')
@@ -19,7 +19,7 @@ $tf->beforeEach(function ($tf) {
     
     tableMySql()->drop();
 
-    tableMySql()->column('Id', 'INTEGER', 'PRIMARY AUTOINCREMENT')
+    tableMySql()->column('Id', 'INTEGER', 'AUTOINCREMENT')
         ->column('FirstName', 'STRING')
         ->column('MiddleNames', 'STRING')
         ->column('LastName', 'STRING')
