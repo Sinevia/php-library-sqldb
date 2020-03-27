b90540d745ee287d7b66150d3dcf2a55bae437c0
Sinevia <info@sinevia.com>
2020-03-27 08:08:44 +0000

    Updates

---
+++ tests/MysqlTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class DivTest extends \PHPUnit\Framework\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+    public function testInit(){
+        $this->asertTrue(true);
+    }
+}
